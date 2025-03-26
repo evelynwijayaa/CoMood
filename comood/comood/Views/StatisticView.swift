ea552e98bf598159372e99e7a13bdbc2396486d6
shierlyy <125436224+shierlyy@users.noreply.github.com>
2025-03-26 09:51:02 +0700

    TabBar

---
+++ comood/comood/Views/StatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticView.swift
+//  comood
+//
+//  Created by Shierly Anastasya Lie on 26/03/25.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    var body: some View {
+        Text("This is statistic")
+    }
+}
+
+#Preview {
+    StatisticView()
+}
--- comood/comood/comoodApp.swift
+++ comood/comood/comoodApp.swift
@@ -13,7 +13,7 @@ struct comoodApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
